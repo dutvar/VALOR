9b0b9c32248f27cd133e85fc12c2dbf0eada672b
Varun Dutt <94442581+dutvar@users.noreply.github.com>
2023-06-17 02:19:21 +0100

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,8 @@
-pip install tensorboardx
-pip install toolz
-pip install ipdb
-pip install matplotlib
-pip install pytorch_pretrained_bert
-pip install easydict
-pip install pyyaml
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./apex
+pip install tensorboardx -q
+pip install toolz -q
+pip install ipdb -q
+pip install matplotlib -q
+pip install pytorch_pretrained_bert -q
+pip install easydict -q
+pip install pyyaml -q
